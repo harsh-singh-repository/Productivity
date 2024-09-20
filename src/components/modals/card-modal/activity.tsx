6651ef49f629cd705e3fd2b3ecf879c4ec464dae
Harsh Singh <hk5425901@gmail.com>
2024-09-20 16:13:24 +0530

    Fixed bugs

--- src/components/modals/card-modal/activity.tsx
+++ src/components/modals/card-modal/activity.tsx
@@ -32,15 +32,15 @@ export const Activity = ({items}:activityProps) =>{
 }
 
 Activity.Skeleton = function ActivitySkeleton(){
-    return(
-        <>
-          <div className="flex items-start gap-x-3 w-full">
-                <Skeleton className="h-6 w-6 bg-neutral-200"/>
-                <div className="w-full">
-                    <Skeleton className="w-24 h-6 mb-2 bg-neutral-200"/>
-                    <Skeleton className="w-full h-10 bg-neutral-200"/>
-                </div>
-          </div>
-        </>
-    )
+  return(
+      <>
+        <div className="flex items-start gap-x-3 w-full">
+              <Skeleton className="h-6 w-6 bg-neutral-200"/>
+              <div className="w-full">
+                  <Skeleton className="w-24 h-6 mb-2 bg-neutral-200"/>
+                  <Skeleton className="w-full h-10 bg-neutral-200"/>
+              </div>
+        </div>
+      </>
+  )
 }
