f3995a3a66a99edb0295ad58ceb1963eaa004ad3
Harsh Singh <hk5425901@gmail.com>
2024-09-20 20:15:49 +0530

    Fixed bugs

--- src/app/(platform)/(dashboard)/organisation/[organisationId]/layout.tsx
+++ src/app/(platform)/(dashboard)/organisation/[organisationId]/layout.tsx
@@ -1,23 +1,14 @@
 import { auth } from "@clerk/nextjs/server";
 import { OrgControl } from "./_components/OrgControl";
 import { startCase } from "lodash";
-import { Metadata } from "next"; // Import Metadata type
 
-export async function generateMetadata(): Promise<Metadata> {
-  const { orgSlug } = auth();
-
-  return {
-    title: startCase(orgSlug || "organization"),
-  };
+const organisationIdLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+     <>
+          <OrgControl/>
+          {children}
+     </>
+  )
 }
 
-const OrganisationIdLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <>
-      <OrgControl />
-      {children}
-    </>
-  );
-};
-
-export default OrganisationIdLayout;
+export default organisationIdLayout;
