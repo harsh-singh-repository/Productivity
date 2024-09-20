8e552a2c2d6bb97dfb038e3bf697db54cc4e474f
Harsh Singh <hk5425901@gmail.com>
2024-09-20 20:30:27 +0530

    Fixed bugs

--- src/app/(platform)/(dashboard)/organisation/[organisationId]/activity/page.tsx
+++ src/app/(platform)/(dashboard)/organisation/[organisationId]/activity/page.tsx
@@ -1,23 +1,24 @@
-import React, { Suspense } from 'react'
-import { Info } from '../_components/info';
-import { Separator } from '@/components/ui/separator';
-import { ActivityList } from '../_components/ActivityList';
+import { Suspense } from "react";
 
+import { Separator } from "@/components/ui/separator";
 
-const ActivityPage = () => {
+import { Info } from "../_components/info";
+
+import { checkSubscription } from "@/lib/subscription";
+import { ActivityList } from "../_components/ActivityList";
+
+const ActivityPage = async () => {
+  const isPro = await checkSubscription();
+
   return (
-    <>
-        <div className="w-full">
-            <Info/>
-            <Separator className='my-2'/>
-            <Suspense fallback={<ActivityList.Skeleton/>}>
-               <ActivityList/>
-            </Suspense>
-        </div>
-    </>
-  )
-}
-
-
+    <div className="w-full">
+      <Info isPro={isPro} />
+      <Separator className="my-2" />
+      <Suspense fallback={<ActivityList.Skeleton />}>
+        <ActivityList/>
+      </Suspense>
+    </div>
+  );
+};
 
 export default ActivityPage;
